6dc029114a4fcf05e753dbb3ca25d506a2ece6b5
Jason Blazzard <blazzard@gmail.com>
2018-08-02 08:43:15 -0600

    deleted 336 folder

--- 336/common/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-                <hr>
-                &copy;<?php echo date("Y"); ?> ACME, All rights reserved.<br>
-                All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br>
-                <?php echo "Last modified: " . date ("F d, Y", getlastmod()); ?>
-            </footer>
